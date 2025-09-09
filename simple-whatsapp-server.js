2234f46d341c0ad176cb11ef2228ee0e6cb07610
beyza sarı <hikmet@texmart.com>
2025-09-10 01:32:11 +0300

    🚀 Railway deploy optimization: Enhanced Puppeteer config & Procfile

--- simple-whatsapp-server.js
+++ simple-whatsapp-server.js
@@ -121,7 +121,16 @@ app.post('/instance/create', async (req, res) => {
             }),
             puppeteer: {
                 headless: true,
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--disable-accelerated-2d-canvas',
+                    '--no-first-run',
+                    '--no-zygote',
+                    '--single-process',
+                    '--disable-gpu'
+                ]
             }
         });
 
